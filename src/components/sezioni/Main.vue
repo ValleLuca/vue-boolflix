e6d62cc868bc514ff9d5a5110a17967f000af92e
ValleLuca <valle.lucaAb@outlook.it>
2022-02-07 16:26:38 +0100

    Cambio box Funzionante

--- src/components/sezioni/Main.vue
+++ src/components/sezioni/Main.vue
@@ -3,81 +3,24 @@
     <div class="row row-cols-5">
       <div class="col border d-flex flex-column justify-content-center text-center" v-for="(element, index) in ArTot" :key="index">
         <!-- caselle -->
-        <div @mouseover="active = true" @mouseleave="active = false">
-          <div v-if="active">
-            <h5>{{ element.titolo }}</h5>
-            <h6>{{ element.titolo_originale }}</h6>
-          </div>
-          <div v-if="active == false">
-            <img class="img-fluid" v-show!="ho" :src="'https://image.tmdb.org/t/p/w500/' + element.immagine">
-          </div>
-          <div v-if="active">
-            <img class="img-fluid" :src="getFlag( element.lingua )">
-            <div class="d-flex justify-content-center">
-              <div v-for="(dVoto, i) in ArTot" :key="i">
-                <div v-if="i <= 4">
-                  <div v-if="element.voto > i">
-                    <i class="fas fa-star"></i>
-                  </div>
-                  <div v-else>
-                    <i class="far fa-star"></i>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <BoxFilmSerie :info="element" :arrBoxTot="ArTot"/>
       </div>
     </div>
   </section>
 </template>
 
 <script>
+import BoxFilmSerie from './boxFilmSerie.vue'
 
 export default {
   name: 'Main',
+  components: {
+    BoxFilmSerie,
+  },
   props: {
     ArTot: Array
-  },
-  data(){
-    return{
-      active: false
-    }
-  },
-  methods: {
-    getFlag( language )
-    {
-      if(language == "en"){
-        language = "gb"
-      }
-      if (language == "ja") {
-        language = "jp"
-      }
-      if(language == "hi")
-      {
-        language = "in"
-      }
-      if(language == "el")
-      {
-        language = "gr"
-      }
-      if(language == "da")
-      {
-        language = "dk"
-      }
-      if(language == "ko")
-      {
-        language = "kr"
-      }
-      if(language == "cs")
-      {
-        language = "cz"
-      }
-      return 'https://countryflagsapi.com/png/' + language
-    },
   }
 }
-//Math.ceil ciò che ci serve 
 </script>
 
 <style scoped lang="scss">
---
+++ src/components/sezioni/boxFilmSerie.vue
@@ -0,0 +1,78 @@
+<template>
+  <div @mouseover="active = true" @mouseleave="active = false">
+    <div v-if="active">
+        <h5>{{ info.titolo }}</h5>
+        <h6>{{ info.titolo_originale }}</h6>
+    </div>
+    <div v-if="!active">
+        <img class="img-fluid" :src="'https://image.tmdb.org/t/p/w500/' + info.immagine">
+    </div>
+    <div v-else>
+        <img class="img-fluid" :src="getFlag( info.lingua )">
+        <div class="d-flex justify-content-center">
+            <div v-for="(dVoto, i) in arrBoxTot" :key="i">
+                <div v-if="i <= 4">
+                    <div v-if="dVoto.voto > i">
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <div v-else>
+                        <i class="far fa-star"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: "boxFilmSerie",
+    props: {
+    info: Object,
+    arrBoxTot: Array
+
+    },
+    data(){
+    return{
+      active: false
+        }
+    },
+    methods:{
+    getFlag( language )
+    {
+      if(language == "en"){
+        language = "gb"
+      }
+      if (language == "ja") {
+        language = "jp"
+      }
+      if(language == "hi")
+      {
+        language = "in"
+      }
+      if(language == "el")
+      {
+        language = "gr"
+      }
+      if(language == "da")
+      {
+        language = "dk"
+      }
+      if(language == "ko")
+      {
+        language = "kr"
+      }
+      if(language == "cs")
+      {
+        language = "cz"
+      }
+      return 'https://countryflagsapi.com/png/' + language
+    }
+    }
+}
+</script>
+
+<style>
+
+</style>
